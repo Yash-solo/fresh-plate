4140e07cb69da58dfd6e9b7dd3ed4fdbc44a4c89
yashraj <yashrajb916@gmail.com>
2026-08-12 18:16:28 +0530

    Make scrollable sidebar

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const burger = document.querySelector('#menubar');
 const cross = document.querySelector('#cutbar');
 const sidebar = document.querySelector('.sidebar');
 burger.addEventListener('click',()=>{
-    sidebar.style.display = 'flex';
+    sidebar.classList.remove('left-0');
+    sidebar.classList.toggle('-left-full');
+    sidebar.classList.toggle('left-0');
+    sidebar.classList.remove('-left-full')
     cross.style.display = "flex";
     burger.style.display = "none";
 })
 
 cross.addEventListener('click',()=>{
-    sidebar.style.display = "none";
+    sidebar.classList.remove('left-0');
+    sidebar.classList.toggle('-left-full');
     cross.style.display = "none";
     burger.style.display = "flex";
 })
