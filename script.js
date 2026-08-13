d93b53ac7e52fdd75fc3eae4cf012c6bf4350143
yashraj <yashrajb916@gmail.com>
2026-08-13 16:37:38 +0530

    Make add item section

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector('#menubar');
 const cross = document.querySelector('#cutbar');
 const sidebar = document.querySelector('.sidebar');
+const midSection = document.querySelector('.midsection');
+const addItems = document.querySelector('.additem');
 burger.addEventListener('click',()=>{
     sidebar.classList.remove('left-0');
     sidebar.classList.toggle('-left-full');
@@ -17,3 +19,34 @@ cross.addEventListener('click',()=>{
     burger.style.display = "flex";
 })
 
+addItems.addEventListener('click',()=>{
+    midSection.style.backgroundColor = "seagreen";
+    midSection.innerHTML = `
+    <form class="flex flex-col gap-4">
+        <h1 class="font-bold text-2xl text-center bg-white p-3 rounded-2xl">Add Item</h1>
+        <div class="flex  flex-col  items-center p-5 bg-amber-50 border-gray-300 border rounded-[20px]">
+            <ul class="grid gap-5 text-[20px]">
+                <li>Enter your item name</li>
+                <input id="item" class="h-8 px-2" type="text"placeholder="ex:- Tomato">
+                <li>In how many days your item will expire?</li>
+                <input id="exdate" class="h-8 px-2" type="number" placeholder="ex:- 3">
+                <button type="button" id="addBtn"class="cursor-pointer bg-amber-300 h-10 rounded-[5px]">Add item</button>
+            </ul>
+        </div>
+    </form>`
+    const AddBtn = document.querySelector('#addBtn');
+
+    let items = JSON.parse(localStorage.getItem('frige'))||[];
+    AddBtn.addEventListener('click',()=>{
+        let item = document.querySelector('#item');
+        let expireD = document.querySelector('#exdate');
+        items.push({itm : item.value ,ex : expireD.value });
+
+        //content clear
+        item.value="";
+        expireD.value = "";
+
+        localStorage.setItem('frige',JSON.stringify(items));
+        alert("Your item added succesfully");
+    })
+})
